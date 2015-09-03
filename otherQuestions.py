3c04f4bfcbc6792930975dc931a9ceede8e6bcb6
Samuel Factor <sfxfactor@gmail.com>
2015-09-03 16:06:26 -0500

    added orbital params and sources. changed printing to output file. added calculation of time of transit/contact.'

--- otherQuestions.py
+++ otherQuestions.py
@@ -7,19 +7,21 @@ AU = 1.49597871e13 #cm
 Msun = 1.989e33 #g
 Mjup = 1.89813e30 #g
 Day = 86164.0916 #seconds in a day
+Rjup = 7.1492e9 #radius of jupiter in cm
+Rs = 6.9599e10 #radius of sun in cm
 
 #####Q2
 
 # HD 80606 b orbit parameters from http://exoplanet.eu/catalog/hd_80606_b/#6832
 # updated Aug. 16, 2014
 
-t0 = 2454424.857*Day #JD
+t0 = 2454424.852*Day #JD
 a = 0.449 * AU
-e = 0.93366
-i = np.radians(89.285)
+e = 0.9332
+i = np.radians(89.32)
 W = np.radians(160.98) # or -19.02 http://adsabs.harvard.edu/abs/2014AAS...22341102W
-w = np.radians(300.651)
-m1 = 0.98 * Msun
+w = np.radians(300.80)
+m1 = 0.97 * Msun
 m2 = 3.94 * Mjup
 
 t1 = 2457235.5 #JD of Sep 1 2015 
@@ -41,17 +43,24 @@ plt.xlabel("Julain date [days]")
 plt.ylabel("Radial Velocity [m/s]")
 plt.title("HD 80606 b")
 plt.savefig("Q2.pdf")
-print "Extreme neg vel on: ", JDs[argrelextrema(Rvs,np.less)]
-print "Extreme pos vel on: ", JDs[argrelextrema(Rvs,np.greater)]
 
+f = open('results.out','w')
+f.write("Extreme neg vel on: {}\n".format(JDs[argrelextrema(Rvs,np.less)]))
+f.write("Extreme pos vel on: {}\n".format(JDs[argrelextrema(Rvs,np.greater)]))
+
+'''
 plt.clf()
 plt.plot(JDs ,obs[0]/AU)
 plt.xlabel("Julain date [days]")
 plt.ylabel("Projected seperation [AU]")
 plt.title("HD 80606 b")
-print "minimum projected seperation on JD "+str(JDs[argrelextrema(obs[0],np.less)])
 plt.savefig("Q3.pdf")
+'''
 
+f.write("minimum projected seperation on JD {}\n".format(JDs[argrelextrema(obs[0],np.less)]))
 
+rpl = 0.98*Rjup
+rs = 0.978*Rs
+f.write("Contact occurs on JD {}\n".format(JDs[argrelextrema(np.abs(obs[0]-rpl-rs),np.less)]))
 
-
+f.close()
