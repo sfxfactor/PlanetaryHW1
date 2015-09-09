e185ae3c7414b8649983712a62885dede3499ba6
Samuel Factor <sfxfactor@gmail.com>
2015-09-09 17:46:50 -0500

    added equations of parallax to latex doc, added real position to first plot - need to add for 2 and 3. should check errors - seem a little big.

--- Q4.py
+++ Q4.py
@@ -37,31 +37,39 @@ err = gp*np.ones(100)
 dt = 5.*365.256*Day
 samples = np.random.random_sample(size=100)*dt 
 samples = np.sort(samples)
+alltimes = np.linspace(0.,1.,num=1000)*dt + 2456863.5*Day
+alltimesJDs = alltimes/Day
 epochs = samples + 2456863.5*Day #July 25 2014 Gaia's first science day
 JDs = epochs/Day
 
 Gobs=HD80606b.calcObs(epochs)
-Rcm, PAcm = [Gobs[2], Gobs[3]]
+obs=HD80606b.calcObs(alltimes)
+GRcm, GPAcm = [Gobs[2], Gobs[3]]
+Rcm, PAcm = [obs[2], obs[3]]
+GDRA, GDDEC = (GRcm/(AU*D))*[-np.cos(GPAcm),np.sin(GPAcm)]
 DRA, DDEC = (Rcm/(AU*D))*[-np.cos(PAcm),np.sin(PAcm)]
 
 ## Gaussian random noise
-RAnoise = np.random.normal(loc=0.0, scale=gp, size=100)
-DECnoise = np.random.normal(loc=0.0, scale=gp, size=100)
-DRA += RAnoise
-DDEC += DECnoise
+GRAnoise = np.random.normal(loc=0.0, scale=gp, size=100)
+GDECnoise = np.random.normal(loc=0.0, scale=gp, size=100)
+GDRA += GRAnoise
+GDDEC += GDECnoise
 
 plt.clf()
 f, axarr = plt.subplots(2, sharex=True)
-axarr[0].errorbar(JDs,DRA*1e6,yerr=err*1e6,fmt='k.')
+axarr[0].errorbar(JDs,GDRA*1e6,yerr=err*1e6,fmt='k.')
+axarr[0].plot(alltimesJDs,DRA*1e6)
 axarr[0].set_title('Reflex motion')
 axarr[0].set_ylabel(r"$\Delta\alpha$ [$\mu as$]")
-axarr[1].errorbar(JDs,DDEC*1e6,yerr=err*1e6,fmt='k.')
+axarr[1].errorbar(JDs,GDDEC*1e6,yerr=err*1e6,fmt='k.')
+axarr[1].plot(alltimesJDs,DDEC*1e6)
 axarr[1].set_ylabel(r"$\Delta\delta$ [$\mu as$]")
 plt.xlabel("Julian date [days]")
 plt.savefig("1time.pdf")
 
 plt.clf()
-plt.errorbar(DRA*1e6,DDEC*1e6,xerr=err*1e6,yerr=err*1e6,fmt='k.')
+plt.errorbar(GDRA*1e6,GDDEC*1e6,xerr=err*1e6,yerr=err*1e6,fmt='k.')
+plt.plot(DRA*1e6,DDEC*1e6)
 plt.title('Reflex motion')
 plt.xlabel(r"$\Delta\alpha$ [$\mu as$]")
 plt.ylabel(r"$\Delta\delta$ [$\mu as$]")
